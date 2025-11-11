3522a730b060a8e60a78a52cdb4dc0d7389de923
JoaoVictorPires <joaovictorsantos0213@gmail.com>
2025-11-11 08:52:57 -0300

    rm directory

--- backend/tests/integration/testDb.ts
+++ backend/tests/integration/testDb.ts
@@ -10,8 +10,4 @@ export async function seedMinimal() {
     const user = await prisma.user.create({ data: { name: 'Ana', email: 'ana@ex.com' } })
     const category = await prisma.category.create({ data: { name: 'Work' } })
     return { user, category }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> cf17de85e40b68240548b7827a3b962c8205dee3
